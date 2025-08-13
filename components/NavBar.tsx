93dcf98dae7863d672276bc720e5c65a63cf0ee7
Pritam Nandy <pritamimca1003@gmail.com>
2025-08-13 21:33:06 +0530

    work

---
+++ components/NavBar.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import React from 'react'
+import { usePathname } from 'next/navigation'
+
+import { Separator } from '@radix-ui/react-separator'
+import { SidebarTrigger } from './ui/sidebar'
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator } from './ui/breadcrumb'
+
+
+const NavBar = () => {
+    const pathname = usePathname();
+
+    // Split the pathname 
+    const segments = pathname.split('/').filter(segment => segment !== '')
+     
+  return (
+    <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+    <div className="flex items-center gap-2 px-3">
+      <SidebarTrigger />
+      <Separator orientation="vertical" className="mr-2 h-4" />
+      <Breadcrumb >
+        <BreadcrumbList className='lg:text-base text-xs font-sans'>
+          <BreadcrumbItem className="hidden md:block">
+            <BreadcrumbLink href="/">
+             {'Dashboard'}
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator className="hidden md:block" />  
+          {
+            segments.map((segment, index)=>(
+            <React.Fragment key={index}>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href={pathname}>
+                    {segment}
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" /> 
+            </React.Fragment> 
+            ))
+          }       
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  </header>
+  )
+}
+
+export default NavBar
---
+++ constants/images.ts
@@ -0,0 +1,1 @@
+export const LOGO_IMG = '/images/logo.svg'
